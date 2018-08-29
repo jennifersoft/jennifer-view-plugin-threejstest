8d7f0f57d9f4a86806e08e7ddf8ec08826713e68
seogi1004 <seogi777@gmail.com>
2018-08-29 11:26:27 +0900

    simple code fixed.

--- src/main/java/com/aries/tutorial/TutorialController.java
+++ src/main/java/com/aries/tutorial/TutorialController.java
@@ -5,6 +5,7 @@ import com.aries.extension.util.ConfigUtil;
 import com.aries.extension.util.LogUtil;
 import com.aries.extension.util.PropertyUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ public class TutorialController extends PluginController {
 
     @RequestMapping(value = { "/tutorial" }, method = RequestMethod.GET)
     @ResponseBody
-    public ModelAndView getMainPage(@RequestParam(required=false, defaultValue="") String layout) {
+    public ModelAndView getMainPage(Model model, @RequestParam(required=false, defaultValue="") String layout) {
         // TODO: layout 매개변수에 따라 다른 템플릿을 적용한다.
         ModelAndView mav = new ModelAndView(layout.equals("iframe") ? "templates/iframe.vm" : "templates/main.vm");
 
