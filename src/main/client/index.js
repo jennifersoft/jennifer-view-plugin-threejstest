6cb93af4138bb8470cf2daf0f77dd38fbce9b9f9
seogi1004 <seogi777@gmail.com>
2018-10-02 20:38:25 +0900

    1.1.0 version released

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -1,6 +1,7 @@
 import styles from './index.css'
 import createSampleChart from './sample.js'
 import $ from 'jquery'
+import extension from 'aries-extension-js'
 
 $(function() {
     let threeObj = createSampleChart();
@@ -12,14 +13,14 @@ $(function() {
     });
 
     // TODO: 제니퍼 서버 주소와 API 사용시 토큰 값을 설정한다.
-    aries.extension.setup({
+    extension.setup({
         hostName: "https://dev.jennifersoft.com",
         apiToken: "6tXrtSu5i8T"
     });
 
     $("#xview_popup").on("click", function(e) {
         // TODO: 다이렉트로 제니퍼의 X-View 팝업을 띄우는 함수이다.
-        aries.extension.popup("xview", {
+        extension.popup("xview", {
             domainId: 7908,
             txIds: [ "-6371365836736069843", "6541742202344215657", "-3416726780880622050" ],
             startTime: 1535462614471,
@@ -29,13 +30,13 @@ $(function() {
 
     $("#active_popup").on("click", function(e) {
         // TODO: 다이렉트로 제니퍼의 액티브서비스 팝업을 띄우는 함수이다.
-        aries.extension.popup("activeService", {
+        extension.popup("activeService", {
             domainId: 7908
         });
     });
 
     // TODO: 제니퍼 Open API를 쉽게 조회할 수 있는 함수이다.
-    aries.extension.api("instance", {
+    extension.api("instance", {
         domain_id: 7908
     }, function(res) {
         console.log(res);
